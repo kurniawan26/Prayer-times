cb402cae9fb1fbaa785028ed588f22f30b213e01
kurniawan26 <kirigaya.kurniawan@gmail.com>
2020-12-07 22:11:30 +0700

    menambah table untuk jadwal sholat

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ function prayerTimes(latitude, longitude){
     fetch('http://api.aladhan.com/v1/calendar?latitude='+latitude+'&longitude='+longitude+'&method=2')
     .then(response => response.json())
     .then(function(response){
-        let date = new Date();
-        let today = date.getDate() -1;
-        // console.log(response.data[today]);
+        let date        = new Date();
+        let today       = date.getDate() -1;
+        let data        = response.data[today].timings;
+
+        let app         = document.getElementById('app')
+        let table       = document.createElement('table');
+        let tableTbody  = document.createElement('tbody');
+
+        for(i in data){
+            let row              = tableTbody.insertRow();
+            let name             = row.insertCell(0);
+            let time             = row.insertCell(1);
+                name.innerHTML   = i;
+                time.innerHTML   = data[i];
+                tableTbody.appendChild(row);
+        }
+        table.appendChild(tableTbody);
+        app.appendChild(table);
     });
 }
 
@@ -25,8 +40,8 @@ function userLocation(){
 }
 
 function index(){
-    let app = document.getElementById('app');
-    let h3  = document.createElement('h3');
+    let app      = document.getElementById('app');
+    let h3       = document.createElement('h3');
     h3.innerHTML = 'Prayer Times';
 
     app.appendChild(h3);
